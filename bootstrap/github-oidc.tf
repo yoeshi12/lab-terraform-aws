9ca454e684b8e7c0ec585391e696eca2a23598e6
Yosselyn Hidalgo <yoeshi.12.c@gmail.com>
2026-09-09 16:47:48 +0000

    Etapa 5.2: autenticación OIDC para GitHub Actions

---
+++ bootstrap/github-oidc.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_version = ">= 1.11"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+variable "github_repo" {
+  description = "Repositorio autorizado en formato usuario/repositorio"
+  type        = string
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-terraform-lab"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "admin_temporal" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "role_arn" {
+  value = aws_iam_role.github_actions.arn
+}
